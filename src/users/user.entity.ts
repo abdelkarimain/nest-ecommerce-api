3786728f4f42ff45b497a5d6068290d498972ec2
Abdelkarim Ain <162830944+abdelkarimain@users.noreply.github.com>
2024-09-14 17:03:28 +0100

    Set Up User Entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string; // 'admin' or 'customer'
+
+  @Column({ default: '' })
+  profile: string;
+}
